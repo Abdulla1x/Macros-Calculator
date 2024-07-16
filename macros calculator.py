fbc09bc15aa4a1a6dcb31264538c4357f742dab4
Abdulla1x <M.abdulla992003@gmail.com>
2024-07-16 18:37:39 +0400

    Implemented the feature for calculating the macros of individual ingredients

--- macros calculator.py
+++ macros calculator.py
@@ -1,9 +1,39 @@
-weight = float(input("Enter the weight in grams: "))
-serving_size = float(input("Enter the serving size: "))
-calories = float(input("Enter the calories per serving: "))
-protein = float(input("Enter the amount of protein per serving: "))
+print("Are you calculating the macros of an ingredient or a meal?")
+selection = input("Enter 'I' for Ingredient or 'M' for meal: ")
 
-total_calories = weight/serving_size*calories
-total_protein = weight/serving_size*protein
+if selection == 'M' or selection == 'm':
+    
+    weight = float(input("Enter the weight in grams: "))
+    serving_size = float(input("Enter the serving size: "))
+    calories = float(input("Enter the calories per serving: "))
+    protein = float(input("Enter the amount of protein per serving: "))
 
-print("Calories: ", total_calories, "\n Protein: ", total_protein)
+    total_calories = weight/serving_size*calories
+    total_protein = weight/serving_size*protein
+
+    print("Calories: ", total_calories, "\nProtein: ", total_protein)
+
+elif selection == 'I' or selection == 'i':
+    terminate = False
+    total_calories = 0
+    total_protein = 0
+    while not terminate:
+        weight = float(input("Enter the weight in grams: "))
+        serving_size = float(input("Enter the serving size: "))
+        calories = float(input("Enter the calories per serving: "))
+        protein = float(input("Enter the amount of protein per serving: "))
+            
+        current_calories = weight/serving_size*calories
+        current_protein = weight/serving_size*protein
+        total_calories += current_calories
+        total_protein += current_protein
+
+        print("Calories (Ingredient): ", current_calories, "\nProtein (Ingredient): ", current_protein)
+
+        print("Do you want to add another ingredient?")
+        addition = input("Enter 'Y' for yes or 'N' for no: ")
+        if addition == 'Y' or addition == 'y':
+            terminate = False
+        elif addition == 'N' or addition == 'n':
+            print("Total Calories: ", total_calories, "\nTotal Protein: ", total_protein)
+            terminate = True
